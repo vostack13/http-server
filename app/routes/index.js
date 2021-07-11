bb64d1e7641902dcd1d7c278255c74b643f6444d
Stanislav Vozikov <svozikov@apple.com>
2021-07-11 20:17:10 +0300

    Remove output dir from repo

--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-var tasks_1 = require("../entries/tasks");
-var tasks_2 = require("./tasks");
-exports.routes = {
-    "/tasks": tasks_2.tasks(new tasks_1.TasksEntry()),
-};
